95cd6c1c6aab6a24c9ef6c5735c100f1cf2fe665
TobiOneCanobi <vlogkongen@gmail.com>
2024-08-28 11:20:12 +0200

    Student part of assigment

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
 import app.DAOs.PersonDAO;
+import app.DAOs.StudentDAO;
 import app.entities.Person;
+import app.entities.Student;
+
+import java.time.LocalDate;
 
 public class Main
 {
@@ -9,14 +13,36 @@ public class Main
     {
         System.out.println("Hello world!");
 
-        PersonDAO personDAO = new PersonDAO();
-        personDAO.create(
-                Person.builder()
+        StudentDAO studentDAO = new StudentDAO();
+//        studentDAO.create(
+//                Student.builder()
+//                        .name("John")
+//                        .phone_number(123456789)
+//                        .email("mail@mail.com")
+//                        .address("Street 1")
+//                        .status("Active")
+//                        .date_of_birth(LocalDate.of(2006,2,12))
+//                        .date_of_enrollment(LocalDate.of(2024,6,1))
+//                        .course_id(1)
+//                        .build()
+//
+//        );
+
+        studentDAO.update(
+                Student.builder()
+                        .id(1)
                         .name("John")
-                        .age(25)
+                        .phoneNumber(123456789)
+                        .email("mail@mail.com")
+                        .address("Street 1")
+                        .status("Active")
+                        .dateOfBirth(LocalDate.of(2006,2,12))
+                        .dateOfEnrollment(LocalDate.of(2024,6,1))
+                        //.course_id(1)
                         .build()
+
         );
-        personDAO.getAllPersons().forEach(System.out::println);
+        studentDAO.getAll().forEach(System.out::println);
 
     }
 }
